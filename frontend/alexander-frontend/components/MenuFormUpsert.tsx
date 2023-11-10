b5ca8d7bab10ac0c8f9bb867c16c8737de2a732a
Alexander Sommer <Alexander.Sommer@akqa.com>
2023-11-10 11:53:53 +0100

    Added supabaseClient to make it more easy to grab and post/put/delete and edit etc.

--- frontend/alexander-frontend/components/MenuFormUpsert.tsx
+++ frontend/alexander-frontend/components/MenuFormUpsert.tsx
@@ -1,5 +1,6 @@
 "use client";
 import { FormEvent } from "react";
+import { supabase } from "@/components/supabaseClient";
 
 export default function MenuUpsert() {
   function handleMenuFormData(event: FormEvent) {
@@ -8,12 +9,34 @@ export default function MenuUpsert() {
 
     const data = {
       title: formData.get("title"),
-      startDate: formData.get("startDate"),
-      endDate: formData.get("endDate"),
-      menuTypeID: formData.get("menuTypeID"),
-      roomID: formData.get("roomID"),
+      start_date: formData.get("startDate"),
+      end_date: formData.get("endDate"),
+      menu_type_id: parseInt(formData.get("menuTypeID") as string, 10),
+      room_id: parseInt(formData.get("roomID") as string, 10),
       published: formData.get("publishMenu") === "on",
     };
+
+    const insertMenu = async () => {
+      let { data: menu, error } = await supabase.from("menu").insert([
+        {
+          title: data.title,
+          start_date: data.start_date,
+          end_date: data.end_date,
+          menu_type_id: data.menu_type_id,
+          room_id: data.room_id,
+          published: data.published,
+        },
+      ]);
+
+      if (error) {
+        console.log("An error occured!");
+      } else {
+        console.log("Successfully inserted menu!!!");
+      }
+
+      console.log(menu);
+    };
+    insertMenu();
 
     console.log(data);
     event.target.reset();
@@ -27,11 +50,11 @@ export default function MenuUpsert() {
       </div>
       <div className="form-example">
         <label for="startDate">Enter start date: </label>
-        <input type="date" name="startDate" id="startDate" required />
+        <input type="datetime-local" name="startDate" id="startDate" required />
       </div>
       <div className="form-example">
         <label for="endDate">Enter end date: </label>
-        <input type="date" name="endDate" id="endDate" required />
+        <input type="datetime-local" name="endDate" id="endDate" required />
       </div>
       <div className="form-example">
         <label for="menuTypeID">Enter menu type ID: </label>
