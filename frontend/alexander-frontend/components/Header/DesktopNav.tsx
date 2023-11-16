62aac2aaf858e4f2e1ca685d9e1f6b39ae0c862e
Alexander Sommer <Alexander.Sommer@akqa.com>
2023-11-16 14:41:19 +0100

    Saved work for today - will begin adding overall functionality in terms of supabase and etc.

--- frontend/alexander-frontend/components/Header/DesktopNav.tsx
+++ frontend/alexander-frontend/components/Header/DesktopNav.tsx
@@ -1,5 +1,8 @@
 "use client";
 import React, { useState } from "react";
+import { MdPerson, MdSettings } from "react-icons/md";
+import Link from "next/link";
+import pageSettings from "@/data/pageSettings";
 
 type DesktopNavProps = {
   style: string; // Optional. Functionality to add styling to the DesktopNav component if needed.
@@ -7,10 +10,27 @@ type DesktopNavProps = {
 
 const DesktopNav = ({ style }: DesktopNavProps) => {
   return (
-    <nav
-      className={`hidden h-48 border-2 border-ghost_white lg:block ${style}`}
-    >
-      <p>Nav only shows in the header on desktop size!</p>
+    <nav className={`hidden lg:block ${style}`}>
+      <ul className="gap-50 flex items-center">
+        <li>
+          <Link
+            href={pageSettings.account.route}
+            className="gap-5 flex flex-col items-center justify-center"
+          >
+            <MdPerson size={30} />
+            <h3>{pageSettings.account.title_alt}</h3>
+          </Link>
+        </li>
+        <li>
+          <Link
+            href={pageSettings.settings.route}
+            className="gap-5 flex flex-col items-center justify-center"
+          >
+            <MdSettings size={30} />
+            <h3>{pageSettings.settings.title}</h3>
+          </Link>
+        </li>
+      </ul>
     </nav>
   );
 };
---
+++ frontend/alexander-frontend/data/pageSettings.js
@@ -0,0 +1,16 @@
+export default {
+  account: {
+    title: "Account",
+    title_alt: "Profile",
+    route: "/account",
+    desc: "",
+    aria: "",
+  },
+  settings: {
+    title: "Settings",
+    title_alt: "",
+    route: "/settings",
+    desc: "",
+    aria: "",
+  },
+};
