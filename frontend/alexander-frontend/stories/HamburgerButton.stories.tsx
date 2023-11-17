f17ea07eb9afd57efe875115f27b81a97d80f133
Alexander Sommer <Alexander.Sommer@akqa.com>
2023-11-17 10:21:48 +0100

    Added storybook

---
+++ frontend/alexander-frontend/stories/HamburgerButton.stories.tsx
@@ -0,0 +1,19 @@
+import type { Meta, StoryObj } from "@storybook/react";
+
+import HamburgerButton from "../components/Buttons/HamburgerButton";
+
+const meta: Meta<typeof HamburgerButton> = {
+  title: "Buttons/Hamburger Button",
+  component: HamburgerButton,
+};
+
+type Story = StoryObj<HamburgerButton>;
+
+export const Primary: Story = {
+  args: {
+    ariaLabel: "Label that describes what the Hamburger component does in terms of accessibility. ",
+    screen: "mobile-tablet",
+  },
+};
+
+export default meta;
