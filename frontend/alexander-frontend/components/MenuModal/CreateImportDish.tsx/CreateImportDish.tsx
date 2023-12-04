d0deb51fc79ac409b919744dfba19a23c7f0b3de
Alexander Sommer <Alexander.Sommer@akqa.com>
2023-12-04 09:18:55 +0100

    Created Create & Import buttons for the menu modal

--- frontend/alexander-frontend/components/MenuModal/CreateImportDish.tsx/CreateImportDish.tsx
+++ frontend/alexander-frontend/components/MenuModal/CreateImportDish.tsx/CreateImportDish.tsx
@@ -4,8 +4,8 @@ import ImportDish from "./child-components/ImportDish";
 const CreateImportDish = () => {
   return (
     <section className="grid grid-cols-2 gap-25">
-      <CreateDish />
-      <ImportDish />
+      <CreateDish label="Click to create a new dish from scratch" />
+      <ImportDish label="Click to import a dish from the archive" />
     </section>
   );
 };
