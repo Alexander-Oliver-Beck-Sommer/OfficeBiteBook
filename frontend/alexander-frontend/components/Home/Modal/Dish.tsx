465240529dd0c49e831dc6d019337a9fff750f42
Alexander Oliver Beck Sommer <alexanderobsommer@gmail.com>
2024-02-05 05:49:07 +0100

    Fixed border issue - will come back at later point

--- frontend/alexander-frontend/components/Home/Modal/Dish.tsx
+++ frontend/alexander-frontend/components/Home/Modal/Dish.tsx
@@ -60,22 +60,22 @@ const Dish = ({
         }`}
       >
         <div className="overflow-hidden ">
-          <ul className="flex flex-col gap-4 pt-4 lg:gap-4 lg:p-4 lg:py-4">
+          <ul className="flex flex-col gap-8 pt-4 lg:p-4 lg:py-4">
             {/* We basically don't want to render any of the list items if there isn't any data to show */}
             {title && (
-              <li className="border-b-2 border-dark-300 pb-4">
+              <li>
                 <p className="mb-1 text-sm font-medium">Title</p>
                 <p className="text-sm text-grey">{title}</p>
               </li>
             )}
             {subtitle && (
-              <li className="border-b-2 border-dark-300 pb-4">
+              <li>
                 <p className="mb-1 text-sm font-medium">Subtitle</p>
                 <p className="text-sm text-grey">{subtitle}</p>
               </li>
             )}
             {description && (
-              <li className="border-b-2 border-dark-300 pb-4">
+              <li>
                 <p className="mb-1 text-sm font-medium">Description</p>
                 <p className="text-sm text-grey">{description}</p>
               </li>
