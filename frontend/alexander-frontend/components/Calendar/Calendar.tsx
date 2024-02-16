6f2a0728d8625046e773061da33e90359e13ac70
Alexander Oliver Beck Sommer <alexanderobsommer@gmail.com>
2024-02-16 05:10:08 +0100

    Added permission restriction on calendar - only users with the role of admin are allowed to access the calendar and configure menus & dishes.

--- frontend/alexander-frontend/components/Calendar/Calendar.tsx
+++ frontend/alexander-frontend/components/Calendar/Calendar.tsx
@@ -1,7 +1,10 @@
 "use client";
+import { useEffect, useState } from "react";
 import useTimeMachine from "@/hooks/useTimeMachine";
 import NavigationBar from "@/components/Calendar/NavigationBar/NavigationBar";
 import Week from "@/components/Calendar/Week/Week";
+import MessageModal from "../Modals/MessageModal/MessageModal";
+import { createClientComponentClient } from "@supabase/auth-helpers-nextjs";
 
 type CalendarProps = {
   userId?: string;
@@ -19,18 +22,56 @@ export default function Calendar({ userId }: CalendarProps) {
     weekReset,
     currentWeekNumber,
   } = useTimeMachine();
+  const supabase = createClientComponentClient();
+
+  // State to track if user data has been fetched
+  const [userDataFetched, setUserDataFetched] = useState(false);
+
+  const fetchUser = async () => {
+    if (userId) {
+      const { data, error } = await supabase
+        .from("users")
+        .select()
+        .eq("user_id", userId)
+        .eq("user_role", "admin");
+
+      if (error) {
+        console.log("Error fetching user", error);
+        return;
+      }
+
+      setUserDataFetched(data && data.length > 0);
+    }
+  };
+
+  useEffect(() => {
+    fetchUser();
+  }, [userId]);
 
   return (
     <>
-      <NavigationBar
-        jumpBack={weekBackward}
-        weekNumber={currentWeekNumber}
-        jumpForward={weekForward}
-        currentDateHighlight={weekHighlighter}
-        currentDateReset={weekReset}
-        typeToggle={weekTypeSwitcher}
-      />
-      <Week userId={userId} days={week.week_days} type={type} hours={hours()} />
+      {userDataFetched ? (
+        <>
+          <NavigationBar
+            jumpBack={weekBackward}
+            weekNumber={currentWeekNumber}
+            jumpForward={weekForward}
+            currentDateHighlight={weekHighlighter}
+            currentDateReset={weekReset}
+            typeToggle={weekTypeSwitcher}
+          />
+          <Week
+            userId={userId}
+            days={week.week_days}
+            type={type}
+            hours={hours()}
+          />
+        </>
+      ) : (
+        <section className="fill-body pattern flex items-center justify-center px-5 py-10">
+          <MessageModal variant={403} />
+        </section>
+      )}
     </>
   );
 }
