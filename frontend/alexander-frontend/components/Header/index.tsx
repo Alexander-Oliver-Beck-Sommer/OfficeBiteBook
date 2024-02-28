b362efa907d40e37ff164f6c7d81c3b1ab63bc82
Alexander Sommer <Alexander.Sommer@akqa.com>
2024-02-28 13:46:04 +0100

    Remastered Header

--- frontend/alexander-frontend/components/Header/index.tsx
+++ frontend/alexander-frontend/components/Header/index.tsx
@@ -2,57 +2,83 @@
 import { Slant } from "hamburger-react";
 import { useState, useEffect } from "react";
 import Link from "next/link";
-import NavigationMenu from "./NavigationMenu";
-import HamburgerMenu from "@/components/Header/HamburgerMenu";
+import ComputerMenu from "./ComputerMenu";
+import MobileMenu from "./MobileMenu";
 import useUtilities from "@/hooks/useUtilities";
 import useSupabaseUsers from "@/hooks/useSupabaseUsers";
 
 const Header = () => {
-  const [isHamburgerOpen, setIsHamburgerOpen] = useState(false);
+  const [visibility, setVisibility] = useState<boolean>(false);
+  const [user, setUser] = useState<boolean>(false);
   const { disableBodyScroll } = useUtilities();
-  const [isLoggedIn, setIsLoggedIn] = useState<false | null>(false);
   const { fetchUserFromSession } = useSupabaseUsers();
 
+  // Awaits fetchUserFromSession and determines if user is logged in
   const fetchUser = async () => {
     const user = await fetchUserFromSession();
 
     if (user) {
-      setIsLoggedIn(true);
+      setUser(true);
     } else {
-      setIsLoggedIn(false);
+      setUser(false);
     }
   };
 
+  // Check if user is logged in on page load
   useEffect(() => {
     fetchUser();
   }, []);
 
-  disableBodyScroll(isHamburgerOpen);
+  // Disable body scroll when menu is visible
+  disableBodyScroll(visibility);
 
+  // Toggle menu visibility
   const handleBurgerMenu = () => {
-    setIsHamburgerOpen(!isHamburgerOpen);
+    setVisibility(!visibility);
   };
 
+  // Array of links that will be displayed in both menus 📚
+  const links = [
+    {
+      text: "Departments", // Text to display
+      label: "Navigate to departments", // value for aria-label and title
+      path: "/", // Path to navigate to
+      icon: "office", // Icon name - must be provided in the underlined link component
+    },
+    {
+      text: "Profile",
+      label: "Navigate to profile",
+      path: "/profile",
+      icon: "profile",
+    },
+  ];
+
   return (
-    <header className="flex h-header_height justify-center bg-dark-100 px-4 md:px-12">
+    <header className="flex h-header_height items-center justify-center bg-dark-100 px-4 md:px-12">
       <section className="flex w-full max-w-screen-xl items-center justify-between gap-4">
-        <Link href="/" aria-label="Home">
+        <Link
+          href="/"
+          aria-label="Go to departments"
+          title="Go to departments"
+          className="transition-300 text-white outline-0 hover:text-grey focus-visible:text-grey"
+        >
           <h3 className="font-semibold">OfficeBiteBook</h3>
         </Link>
         <div className="lg:hidden">
           <Slant
-            toggled={isHamburgerOpen}
+            toggled={visibility}
             toggle={handleBurgerMenu}
             rounded
             label="Show menu"
           />
         </div>
-        <NavigationMenu />
+        <ComputerMenu user={user} links={links} />
       </section>
-      <HamburgerMenu
-        user={isLoggedIn}
-        visible={isHamburgerOpen}
+      <MobileMenu
+        user={user}
+        visibility={visibility}
         toggle={handleBurgerMenu}
+        links={links}
       />
     </header>
   );
